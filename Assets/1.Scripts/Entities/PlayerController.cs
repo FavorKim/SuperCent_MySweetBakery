28afdd0f1132de2771dfff5b3cd2314c21a16455
FavorKim <kimharangs@gmail.com>
2024-10-23 16:29:31 +0900

    [feat] 플레이어 이동 추가

--- Assets/1.Scripts/Entities/PlayerController.cs
+++ Assets/1.Scripts/Entities/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private ControllStick stick;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotSpeed;
 
 
     private void Update()
@@ -15,9 +16,33 @@ public class PlayerController : MonoBehaviour
 
     private void PlayerMove()
     {
+        Vector3 moveDir = GetMoveDir();
+
+        MoveTowards(moveDir);
+    }
+
+
+
+    private Vector3 GetMoveDir()
+    {
         Vector2 dir = stick.GetInputVector();
         Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
+        return moveDir;
+    }
 
-        transform.Translate(moveDir * Time.deltaTime * moveSpeed);
+    private void MoveTowards(Vector3 direction)
+    {
+
+        if (direction.sqrMagnitude > 0.001f)  // 방향 벡터가 거의 0이 아닌 경우
+        {
+            // Y축만 회전
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+            // 회전값 보간
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotSpeed);
+        }
+
+        // 플레이어 이동
+        transform.position += direction * moveSpeed * Time.deltaTime;
     }
 }
