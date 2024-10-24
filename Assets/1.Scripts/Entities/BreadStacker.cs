42bd087e96f3700aef15a7f468318c62bf5721a8
FavorKim <kimharangs@gmail.com>
2024-10-24 17:11:23 +0900

    [feat] 빵을 쌓는 기능을 BreadStacker로 분할 후 플레이어에 상속
    이 후 Customer가 빵을 쌓는 기능을 사용할 때 상속시킬 예정

---
+++ Assets/1.Scripts/Entities/BreadStacker.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadStacker : MonoBehaviour
+{
+    protected Animator anim;
+
+    protected event Action<Bread> OnPushBread;
+    protected event Action<Bread> OnPopBread;
+
+    private Stack<Bread> breadStack = new Stack<Bread>();
+    private float stackHeight = 0.3f;
+    [SerializeField] private float stackDelay = 0.5f;
+    [SerializeField] protected int stackMaxCount = 8;
+    protected bool isStakcing = false;
+
+    [SerializeField] private Transform stackPos;
+    [SerializeField] private Transform stackStartPos;
+    protected int stackCount => breadStack.Count;
+
+    protected virtual void OnEnable()
+    {
+        OnPushBread += OnPushBread_PushBread;
+        OnPushBread += OnPushBread_PlayStackAnimation;
+        OnPushBread += OnPushBread_SetPlayerAnim;
+
+        OnPopBread += OnPopBread_SetBreadTransform;
+        OnPopBread += OnPopBread_SetPlayerAnim;
+    }
+    protected virtual void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    protected virtual void OnDisable()
+    {
+        OnPushBread -= OnPushBread_SetPlayerAnim;
+        OnPushBread -= OnPushBread_PlayStackAnimation;
+        OnPushBread -= OnPushBread_PushBread;
+
+        OnPopBread -= OnPopBread_SetPlayerAnim;
+        OnPopBread -= OnPopBread_SetBreadTransform;
+    }
+
+    private void OnPopBread_SetBreadTransform(Bread bread)
+    {
+        bread.transform.SetParent(null);
+        bread.OnPopped();
+    }
+    private void OnPopBread_SetPlayerAnim(Bread bread)
+    {
+        anim.SetBool("isStack", false);
+    }
+
+    private void OnPushBread_PlayStackAnimation(Bread bread)
+    {
+        bread.transform.SetParent(stackPos.transform);
+
+        StartCoroutine(CorStackAnim(bread.transform, GetStackStartPos, GetStackDestPos));
+    }
+    private void OnPushBread_PushBread(Bread bread)
+    {
+        bread.OnPushed();
+        breadStack.Push(bread);
+    }
+    private void OnPushBread_SetPlayerAnim(Bread bread)
+    {
+        anim.SetBool("isStack", true);
+    }
+
+
+    protected virtual void OnTriggerStorage(BreadStorage storage)
+    {
+
+    }
+    protected virtual void OnTriggerSaleShelves(SaleShelves shelves)
+    {
+
+    }
+
+
+    public Bread PopBread()
+    {
+        if (breadStack.Count > 0)
+        {
+
+            Bread bread = breadStack.Pop();
+            OnPopBread.Invoke(bread);
+            return bread;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    protected Vector3 GetStackDestPos()
+    {
+        float height = stackHeight * breadStack.Count;
+        Vector3 destPos = new Vector3(stackPos.position.x, stackPos.position.y + height, stackPos.position.z);
+        return destPos;
+    }
+    protected Vector3 GetStackStartPos()
+    {
+        float yGap = breadStack.Count * stackHeight;
+
+        Vector3 startPos = new Vector3(stackStartPos.position.x, stackStartPos.position.y + yGap, stackStartPos.position.z);
+        return startPos;
+    }
+
+    protected void InvokeOnPushBread(Bread bread)
+    {
+        OnPushBread.Invoke(bread);
+    }
+    protected void InvokeOnPopBread(Bread bread)
+    {
+        OnPopBread.Invoke(bread);
+    }
+
+    protected IEnumerator CorStackAnim(Transform bread, Func<Vector3> startPos, Func<Vector3> destPos)
+    {
+        float curTime = 0;
+
+        bread.transform.position = startPos.Invoke();
+        isStakcing = true;
+        yield return null;
+
+        while (curTime < stackDelay)
+        {
+            curTime += Time.deltaTime;
+            yield return null;
+        }
+
+        bread.transform.position = destPos.Invoke();
+        bread.transform.localRotation = Quaternion.identity;
+
+        isStakcing = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent(out BreadStorage storage))
+        {
+            OnTriggerStorage(storage);
+        }
+        if (other.TryGetComponent(out SaleShelves shelves))
+        {
+            OnTriggerSaleShelves(shelves);
+        }
+    }
+
+}
--- Assets/1.Scripts/Entities/PlayerController.cs
+++ Assets/1.Scripts/Entities/PlayerController.cs
@@ -1,50 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 using static UnityEditor.PlayerSettings;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BreadStacker
 {
     [SerializeField] private ControllStick stick;
-    [SerializeField] private Transform stackPos;
-    [SerializeField] private Transform stackStartPos;
+    
 
     private NavMeshAgent agent;
-    private Animator anim;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotSpeed;
 
-    private Stack<Bread> breadStack = new Stack<Bread>();
-    private float stackHeight = 0.3f;
-    [SerializeField] private float stackDelay = 0.5f;
-    [SerializeField] private int stackMaxCount = 8;
-    private bool isStakcing = false;
+    
 
-    private int stackCount => breadStack.Count;
-
-    private event Action<Bread> OnPushBread;
-    private event Action<Bread> OnPopBread;
     private event Action<Vector3> OnMove;
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         agent = GetComponent<NavMeshAgent>();
-        anim = GetComponentInChildren<Animator>();
     }
 
-    private void OnEnable()
+    protected override void OnEnable()
     {
-        OnPushBread += OnPushBread_PushBread;
-        OnPushBread += OnPushBread_PlayStackAnimation;
-        OnPushBread += OnPushBread_SetPlayerAnim;
-
-        OnPopBread += OnPopBread_SetBreadTransform;
-        OnPopBread += OnPopBread_SetPlayerAnim;
-
+        base.OnEnable();
         OnMove += OnMove_MoveTowards;
         OnMove += OnMove_SetPlayerAnim;
     }
@@ -54,15 +39,9 @@ public class PlayerController : MonoBehaviour
         PlayerMove();
     }
 
-    private void OnDisable()
+    protected override void OnDisable()
     {
-        OnPushBread -= OnPushBread_SetPlayerAnim;
-        OnPushBread -= OnPushBread_PlayStackAnimation;
-        OnPushBread -= OnPushBread_PushBread;
-
-        OnPopBread -= OnPopBread_SetPlayerAnim;
-        OnPopBread -= OnPopBread_SetBreadTransform;
-
+        base.OnDisable();
         OnMove -= OnMove_SetPlayerAnim;
         OnMove -= OnMove_MoveTowards;
     }
@@ -86,7 +65,7 @@ public class PlayerController : MonoBehaviour
         }
 
         // 플레이어 이동
-        agent.Move(direction * moveSpeed * Time.deltaTime);
+        agent.Move(direction * Time.deltaTime* moveSpeed);
     }
     private void OnMove_SetPlayerAnim(Vector3 dir)
     {
@@ -94,47 +73,23 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isMove", isMove);
     }
 
-    private void OnPopBread_SetBreadTransform(Bread bread)
-    {
-        bread.transform.SetParent(null);
-        bread.OnPopped();
-    }
-    private void OnPopBread_SetPlayerAnim(Bread bread)
-    {
-        anim.SetBool("isStack", false);
-    }
-
-    private void OnPushBread_PlayStackAnimation(Bread bread)
-    {
-        bread.transform.SetParent(stackPos.transform);
-
-        StartCoroutine(CorStackAnim(bread.transform, GetStackStartPos, GetStackDestPos));
-    }
-    public void OnPushBread_PushBread(Bread bread)
-    {
-        bread.OnPushed();
-        breadStack.Push(bread);
-    }
-    private void OnPushBread_SetPlayerAnim(Bread bread)
-    {
-        anim.SetBool("isStack", true);
-    }
+    
     #endregion
 
 
-    private void OnTriggerStorage(BreadStorage storage)
+    protected override void OnTriggerStorage(BreadStorage storage)
     {
         if (!isStakcing)
         {
             Bread bread = storage.OnPopBread();
             if (bread != null)
             {
-                if (breadStack.Count < stackMaxCount)
-                    OnPushBread.Invoke(bread);
+                if (stackCount < stackMaxCount)
+                    InvokeOnPushBread(bread);
             }
         }
     }
-    private void OnTriggerSaleShelves(SaleShelves shelves)
+    protected override void OnTriggerSaleShelves(SaleShelves shelves)
     {
         if (shelves.IsStackable() && !isStakcing)
         {
@@ -148,20 +103,7 @@ public class PlayerController : MonoBehaviour
     }
     #region Method
 
-    public Bread PopBread()
-    {
-        if (breadStack.Count > 0)
-        {
-
-            Bread bread = breadStack.Pop();
-            OnPopBread.Invoke(bread);
-            return bread;
-        }
-        else
-        {
-            return null;
-        }
-    }
+   
 
     private Vector3 GetMoveDir()
     {
@@ -169,20 +111,7 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
         return moveDir;
     }
-    private Vector3 GetStackDestPos()
-    {
-        float height = stackHeight * breadStack.Count;
-        Vector3 destPos = new Vector3(stackPos.position.x, stackPos.position.y + height, stackPos.position.z);
-        return destPos;
-    }
-    private Vector3 GetStackStartPos()
-    {
-        float yGap = breadStack.Count * stackHeight;
-
-        Vector3 startPos = new Vector3(stackStartPos.position.x, stackStartPos.position.y + yGap, stackStartPos.position.z);
-        return startPos;
-    }
-
+    
     private void PlayerMove()
     {
         Vector3 moveDir = GetMoveDir();
@@ -193,37 +122,9 @@ public class PlayerController : MonoBehaviour
 
     #endregion
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.TryGetComponent(out BreadStorage storage))
-        {
-            OnTriggerStorage(storage);
-        }
-        if (other.TryGetComponent(out SaleShelves shelves))
-        {
-            OnTriggerSaleShelves(shelves);
-        }
-    }
+   
 
 
-    private IEnumerator CorStackAnim(Transform bread, Func<Vector3> startPos, Func<Vector3> destPos)
-    {
-        float curTime = 0;
-
-        bread.transform.position = startPos.Invoke();
-        isStakcing = true;
-        yield return null;
-
-        while (curTime < stackDelay)
-        {
-            curTime += Time.deltaTime;
-            yield return null;
-        }
-
-        bread.transform.position = destPos.Invoke();
-        bread.transform.localRotation = Quaternion.identity;
-
-        isStakcing = false;
-    }
+    
 
 }
